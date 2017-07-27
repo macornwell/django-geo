d84fbc8a30e4dea44d0b6ea317e78f83e96baa0f
Mike Cornwell <mike@bonnieblueag.com>
2017-07-27 13:16:26 -0500

    Fixed 'details' ending.

--- django_geo/urls.py
+++ django_geo/urls.py
@@ -32,10 +32,10 @@ model_details_urls = [
 ]
 
 model_list_urls = [
-    url(r'^api/v1/geocoordinate/(?P<pk>\d+)/$', views.GeoCoordinateDetails.as_view(), name='geocoordinate-details'),
-    url(r'^api/v1/continent/(?P<pk>\d+)/$', views.ContinentDetails.as_view(), name='continent-details'),
-    url(r'^api/v1/country/(?P<pk>\d+)/$', views.CountryDetails.as_view(), name='country-details'),
-    url(r'^api/v1/state/(?P<pk>\d+)/$', views.StateDetails.as_view(), name='state-details'),
-    url(r'^api/v1/zipcode/(?P<pk>\d+)/$', views.ZipcodeDetails.as_view(), name='zipcode-details'),
+    url(r'^api/v1/geocoordinate/(?P<pk>\d+)/$', views.GeoCoordinateDetails.as_view(), name='geocoordinate-detail'),
+    url(r'^api/v1/continent/(?P<pk>\d+)/$', views.ContinentDetails.as_view(), name='continent-detail'),
+    url(r'^api/v1/country/(?P<pk>\d+)/$', views.CountryDetails.as_view(), name='country-detail'),
+    url(r'^api/v1/state/(?P<pk>\d+)/$', views.StateDetails.as_view(), name='state-detail'),
+    url(r'^api/v1/zipcode/(?P<pk>\d+)/$', views.ZipcodeDetails.as_view(), name='zipcode-detail'),
     url(r'^api/v1/location/(?P<pk>[0-9]+)/$', views.LocationDetail.as_view(), name='location-detail'),
 ]
