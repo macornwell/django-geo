6560d2c403df60413ccfe6a465a0dd6a39eae579
Michael Cornwell <mike@gnutech.io>
2017-07-26 19:02:39 -0500

    Added missing files, license and added setup.

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-geo',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GPLv3',  
+    description='A Django app for enabling geo capabilities.',
+    long_description=README,
+    author='Mike Cornwell',
+    author_email='mike@mikecornwell.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
+
