4563ee9b8466ef34346306c0801ed0d77582c9c6
Michael Cornwell <mike@gnutech.io>
2017-07-27 13:18:27 -0500

    Added City urls.

--- django_geo/urls.py
+++ django_geo/urls.py
@@ -26,6 +26,7 @@ model_details_urls = [
     url(r'^api/v1/geocoordinate/', views.GeoCoordinateList.as_view(), name='geocoordinate-list'),
     url(r'^api/v1/continent/', views.ContinentList.as_view(), name='continent-list'),
     url(r'^api/v1/country/', views.CountryList.as_view(), name='country-list'),
+    url(r'^api/v1/city/', views.CityList.as_view(), name='city-list'),
     url(r'^api/v1/state/', views.StateList.as_view(), name='state-list'),
     url(r'^api/v1/zipcode/', views.ZipcodeList.as_view(), name='zipcode-list'),
     url(r'^api/v1/location/', views.LocationList.as_view(), name='location-list'),
@@ -36,6 +37,7 @@ model_list_urls = [
     url(r'^api/v1/continent/(?P<pk>\d+)/$', views.ContinentDetails.as_view(), name='continent-detail'),
     url(r'^api/v1/country/(?P<pk>\d+)/$', views.CountryDetails.as_view(), name='country-detail'),
     url(r'^api/v1/state/(?P<pk>\d+)/$', views.StateDetails.as_view(), name='state-detail'),
+    url(r'^api/v1/city/(?P<pk>\d+)/$', views.CityDetails.as_view(), name='city-detail'),
     url(r'^api/v1/zipcode/(?P<pk>\d+)/$', views.ZipcodeDetails.as_view(), name='zipcode-detail'),
     url(r'^api/v1/location/(?P<pk>[0-9]+)/$', views.LocationDetail.as_view(), name='location-detail'),
 ]
