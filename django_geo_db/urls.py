588d5b9a17bc79923de546705f59d103fede5518
Mike Cornwell <mike@bonnieblueag.com>
2017-08-05 14:49:54 -0500

    Separated out autocomplete logic.

--- django_geo_db/urls.py
+++ django_geo_db/urls.py
@@ -1,27 +1,6 @@
 from django_geo_db import views
 from django.conf.urls import url
 
-autocomplete_urls = [
-    url('^autocomplete/named-location/$',
-        views.NamedLocationAutocomplete.as_view(),
-        name='named-location-autocomplete'),
-    url('^autocomplete/location/$',
-        views.LocationAutocomplete.as_view(),
-        name='location-autocomplete'),
-    url('^autocomplete/public-locations/$',
-        views.PublicLocationsAutocomplete.as_view(),
-        name='public-locations-autocomplete'),
-    url('^autocomplete/zipcode/$',
-        views.ZipcodeAutocomplete.as_view(),
-        name='zipcode-autocomplete'),
-    url('^autocomplete/city/$',
-        views.CityAutocomplete.as_view(),
-        name='city-autocomplete'),
-    url('^autocomplete/geocoordinate/$',
-        views.GeoCoordinateAutocomplete.as_view(),
-        name='geocoordinate-autocomplete'),
-]
-
 model_urls = [
     url(r'^api/v1/geocoordinate/(?P<pk>\d+)/$', views.GeoCoordinateDetails.as_view(), name='geocoordinate-detail'),
     url(r'^api/v1/continent/(?P<pk>\d+)/$', views.ContinentDetails.as_view(), name='continent-detail'),
