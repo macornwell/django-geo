4eca824d82935ff8946317a735a890618faa1eae
Michael Cornwell <mike@gnutech.io>
2018-03-12 09:55:14 -0500

    Added ability to clear map cache.

---
+++ django_geo_db/management/commands/clear_location_maps.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django_geo_db.models import LocationMap
+from django_geo_db.services import get_us_states_boundaries
+from django.core.files.storage import default_storage
+from django.db import transaction
+
+@transaction.atomic
+class Command(BaseCommand):
+    help = "Deletes all Location Maps and clears them from the file system."
+
+    def handle(self, *args, **options):
+        for m in LocationMap.objects.all():
+            url = m.map_file_url
+            if 'static' in url:
+                continue
+            else:
+                print('Deleting Map: {0}'.format(url))
+                default_storage.delete(url)
+            print('Deleting Obj: {0}'.format(m))
+            m.delete()
+
